46aadda413552df831d386db67924325d58a98aa
Soham Datta <83786816+TECH-SAVVY-GUY@users.noreply.github.com>
2023-05-28 15:18:55 +0530

    Update `script.js`

--- static/qrCode/script.js
+++ static/qrCode/script.js
@@ -1,7 +1,16 @@
 Telegram.WebApp.ready()
 Telegram.WebApp.expand()
+Telegram.WebApp.onEvent('qrTextReceived', sendData)
 
-Telegram.WebApp.onEvent('qrTextReceived', function(data) {
-    console.log(this, data);
+function sendData(this, data) {
+  console.log(this, data);
+  fetch('/qrCode-response', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ qr: data, initData: window.Telegram.WebApp.initData })
   });
-  
+};
+
